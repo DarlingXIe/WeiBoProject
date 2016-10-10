e29edd2bb27f3e1eebf9374b86b63201dae37a1d
DalinXie <xdl.tmac@qq.com>
2016-10-10 18:22:55 -0400

    updateVerson emotionKeyBorad

---
+++ WeiBo/WeiBo/Classes/View/compose/view/emotionKeyBoard/XDLEmotionButton.swift
@@ -0,0 +1,21 @@
+//
+//  XDLEmotionButton.swift
+//  WeiBo
+//
+//  Created by DalinXie on 16/10/10.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+import UIKit
+
+class XDLEmotionButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
